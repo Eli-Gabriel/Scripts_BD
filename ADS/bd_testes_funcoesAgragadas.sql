7101b0a743369d55ace24123a2da1d16f1cab6a8
ZalZin <eligabriel2013@gmail.com>
2019-05-09 22:06:24 -0400

    .

--- ADS/bd_testes_funcoesAgragadas.sql
+++ ADS/bd_testes_funcoesAgragadas.sql
@@ -1,4 +1,4 @@
---1 
+﻿--1 
 /*- A média de peso dos pacientes do sexo masculino, alcoolatras;
 select avg(peso) from paciente
 where sexo = 'M' and alcoolatra;
@@ -6,7 +6,10 @@ where sexo = 'M' and alcoolatra;
 
 --2 
 /*- Quantidade de Consultas por paciente;
-select count(*) from consulta group by id_paciente;
+select paciente.nome, count(consulta.*) as contagem 
+from paciente
+join consulta on paciente.id_paciente = consulta.id_paciente
+group by paciente.nome;
 --*/
 
 --3 
@@ -28,45 +31,59 @@ select avg(peso) from consulta
 where data between '01-01-1999' and '01-01-2015';
 --*/
 
---6 DPSDPSDPSDPSDPSDPSDPSDPSDPS
+--6 
 /*- Selecione os tipos sanguíneos no banco que contenham mais de 60 
 --pacientes cadastrados.
-select count(*) as qtd, tipo_sanguineo from paciente 
-where qtd > 60
-group by tipo_sanguineo order by tipo_sanguineo; 
+select tipo_sanguineo, count(tipo_sanguineo) as qtd 
+from paciente
+group by tipo_sanguineo 
+having count(tipo_sanguineo) > 60
 --*/
 
---7 
+--7 DPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPSDPS
 /*- Selecione a média de peso dos pacientes que possuem entre 30 e 35 anos;
-select avg(peso) from paciente 
-where  data_nascimento between '01-01-1984' and '01-01-1989';
+select avg(peso), (EXTRACT(YEAR FROM NOW()) - EXTRACT(YEAR FROM data_nascimento)) AS idade
+from paciente
+having idade between 30 and 35
 --*/
 
---8 DPSDPSDPSDPSDPSDPSDPSDPSDPS
+--8 
 /*- selecione o nome e a quantidade de consultas realizadas por 
 --cada paciente para pacientes que tenham mais de 4 consultas e sejam 
 --do sexo feminino. Ordene seu select pelo número de consultas de forma crescente;
 select paciente.nome, count(consulta.id_consulta) as qtd
 from paciente
 join consulta on paciente.id_paciente = consulta.id_paciente
-where paciente.sexo = 'F' 
-group by paciente.nome 
+where paciente.sexo = 'F'
+group by paciente.nome
+having count(consulta.id_consulta) > 4 
+order by qtd
 --*/
 
---9 DPSDPSDPSDPSDPSDPSDPSDPSDPS
+--9 
 /*- faça uma consulta que mostre os pacientes que ganham próximo da média 
 --de rendimento geral dos pacientes (um intervalo de 10% a mais e a menos);
-select nome,  avg(rendimento) as media
-from paciente where rendimento between (media*1.1) and (media*0.9)
-
+select nome, rendimento
+from paciente
+where rendimento::numeric between 
+(select avg(rendimento::numeric) from paciente)*0.9 and
+(select avg(rendimento::numeric) from paciente)*1.1
 --*/
 
---10 /*- Selecione o nome do paciente que mais participou de consultas.
-select paciente.nome, max((select count(consulta.id_paciente from consulta)))
+--10 
+/*- Selecione o nome do paciente que mais participou de consultas.
+select paciente.nome, count(consulta.id_paciente)
 from paciente 
 join consulta on paciente.id_paciente = consulta.id_paciente
 
+group by paciente.nome
 --*/
 
---11 /*- Selecione o mais novo dentre os pacientes consultados com o médico de id 6;
+--11 
+/*- Selecione o mais novo dentre os pacientes consultados com o médico de id 6
+select paciente.nome, max(paciente.data_nascimento)
+from paciente
+join consulta on paciente.id_paciente = consulta.id_paciente
+where consulta.id_medico = 6
+group by paciente.nome --limit 1
 --*/
